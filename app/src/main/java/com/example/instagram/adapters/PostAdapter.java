4b300521078a1c80366ec26d42e90ac5c681f4f8
Patrice Gill <pgill30@fb.com>
2020-07-08 16:46:26 -0400

    Extended our Adapter from the RecyclerView.Adapter parent class and implemented methods

--- app/src/main/java/com/example/instagram/adapters/PostAdapter.java
+++ app/src/main/java/com/example/instagram/adapters/PostAdapter.java
@@ -1,4 +1,81 @@
 package com.example.instagram.adapters;
 
-public class PostAdapter {
+import android.content.Context;
+import android.gesture.GestureLibraries;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.instagram.R;
+import com.example.instagram.models.Post;
+import com.parse.ParseFile;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
+    private Context context;
+    private List<Post> posts;
+
+    public PostAdapter(Context context, List<Post> posts){
+        this.context = context;
+        this.posts = posts;
+    }
+
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_post, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Post post = posts.get(position);
+        holder.bind(post);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView tvUsername;
+        private ImageView ivImage;
+        private TextView tvDescription;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            //find the views
+            tvUsername = itemView.findViewById(R.id.tvUsername);
+            ivImage = itemView.findViewById(R.id.ivImage);
+            tvDescription = itemView.findViewById(R.id.tvDescription);
+        }
+
+        public void bind(Post post){
+            //bind the post data to the view elements
+            tvDescription.setText(post.getDescription());
+            tvUsername.setText(post.getUser().getUsername());
+            //check if the post has a valid image
+            ParseFile image = post.getImage();
+            if (image != null) {
+                Glide.with(context).load(post.getImage().getUrl()).into(ivImage);
+            }
+
+        }
+
+    }
 }
