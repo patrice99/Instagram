c23117571732b6ac564a9db6b561e3d62a6113ad
Patrice Gill <pgill30@fb.com>
2020-07-09 00:31:38 -0400

    Added PostDetailsActivity and corresponding XML file

---
+++ app/src/main/java/com/example/instagram/activities/PostDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.example.instagram.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PostDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_details);
+    }
+}
