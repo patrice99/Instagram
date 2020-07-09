fb7f73b49514437cd85f4245b6945a01af5a07d8
Patrice Gill <pgill30@fb.com>
2020-07-08 23:45:53 -0400

    Set up SwipeRefreshLayout and set on refresh listener

--- app/src/main/java/com/example/instagram/fragments/PostFragment.java
+++ app/src/main/java/com/example/instagram/fragments/PostFragment.java
@@ -11,6 +11,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.example.instagram.R;
 import com.example.instagram.adapters.PostAdapter;
@@ -28,6 +29,7 @@ public class PostFragment extends Fragment {
     private RecyclerView rvPosts;
     protected PostAdapter adapter;
     protected List<Post> allPosts;
+    private SwipeRefreshLayout swipeContainer;
 
     @Nullable
     @Override
@@ -50,6 +52,21 @@ public class PostFragment extends Fragment {
         //set the layout manager on the recycler view
         rvPosts.setLayoutManager(new LinearLayoutManager(getContext()));
         queryPosts();
+
+        swipeContainer = (SwipeRefreshLayout) view.findViewById(R.id.swipeContainer);
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                //get latest 20 Instagram items
+                queryPosts();
+            }
+        });
+
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
 
     }
 
