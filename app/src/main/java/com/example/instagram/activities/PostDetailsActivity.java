01099230c121579502bb756a7f487589df09c707
Patrice Gill <pgill30@fb.com>
2020-07-09 17:35:45 -0400

    Get the intent and post

--- app/src/main/java/com/example/instagram/activities/PostDetailsActivity.java
+++ app/src/main/java/com/example/instagram/activities/PostDetailsActivity.java
@@ -3,14 +3,23 @@ package com.example.instagram.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.example.instagram.R;
+import com.example.instagram.models.Post;
 
 public class PostDetailsActivity extends AppCompatActivity {
+    Post post;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_details);
+
+        //get the intent and post passed in
+        post = getIntent().getParcelableExtra("post");
+
+        //define Views
+
     }
 }
