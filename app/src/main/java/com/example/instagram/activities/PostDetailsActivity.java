0fe2d61e1d9234dbdb7a481ecdca8766275b65c7
Patrice Gill <pgill30@fb.com>
2020-07-09 23:28:32 -0400

    Added profile pic to DetailsActivity

--- app/src/main/java/com/example/instagram/activities/PostDetailsActivity.java
+++ app/src/main/java/com/example/instagram/activities/PostDetailsActivity.java
@@ -18,6 +18,7 @@ public class PostDetailsActivity extends AppCompatActivity {
     private TextView tvUsername2;
     private TextView tvDescription;
     private TextView tvTimestamp;
+    private ImageView ivProfilePic;
 
 
     @Override
@@ -34,6 +35,7 @@ public class PostDetailsActivity extends AppCompatActivity {
         tvUsername2 = findViewById(R.id.tvUsername2);
         tvDescription= findViewById(R.id.tvDescription);
         tvTimestamp = findViewById(R.id.tvTimestamp);
+        ivProfilePic = findViewById(R.id.ivProfilePic);
 
         tvUsername.setText(post.getUser().getUsername());
         tvUsername2.setText(post.getUser().getUsername());
@@ -46,6 +48,21 @@ public class PostDetailsActivity extends AppCompatActivity {
         if (image != null) {
             Glide.with(this).load(post.getImage().getUrl()).into(ivPostPic);
         }
+
+        //check if the user has a valid profilePic
+        ParseFile image2 = post.getUser().getParseFile("profilePic");
+        if (image2 != null) {
+            Glide.with(this)
+                    .load(post.getUser().getParseFile("profilePic").getUrl())
+                    .circleCrop()
+                    .into(ivProfilePic);
+        } else {
+            Glide.with(this)
+                    .load(getResources().getString(R.string.DEFAULT_PROFILE_PIC))
+                    .circleCrop()
+                    .into(ivProfilePic);
+        }
+
 
 
     }
