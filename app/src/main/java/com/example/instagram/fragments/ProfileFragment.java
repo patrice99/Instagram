498f7e7315b837f859bffc39a4c2b4d3515513ff
Patrice Gill <pgill30@fb.com>
2020-07-09 21:28:43 -0400

    Extended Profile Fragment from Fragment super class instead of from PostFragment super class

--- app/src/main/java/com/example/instagram/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/instagram/fragments/ProfileFragment.java
@@ -1,6 +1,7 @@
 package com.example.instagram.fragments;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,8 +9,14 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import com.example.instagram.EndlessRecyclerViewScrollListener;
 import com.example.instagram.R;
+import com.example.instagram.adapters.PostAdapter;
 import com.example.instagram.models.Post;
 import com.parse.FindCallback;
 import com.parse.ParseException;
@@ -17,9 +24,18 @@ import com.parse.ParseFile;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class ProfileFragment extends PostFragment {
+public class ProfileFragment extends Fragment {
+    private ParseUser filterForUser;
+    private RecyclerView rvUserPosts;
+    private List<Post> userPosts;
+    private PostAdapter userAdapter;
+    private SwipeRefreshLayout swipeContainer;
+    private EndlessRecyclerViewScrollListener scrollListener;
+    private static final int DISPLAY_LIMIT = 20;
+    public static final String TAG = ProfileFragment.class.getSimpleName();
 
 
     @Nullable
@@ -33,10 +49,75 @@ public class ProfileFragment extends PostFragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         filterForUser = ParseUser.getCurrentUser();
         super.onViewCreated(view, savedInstanceState);
+
+        rvUserPosts = view.findViewById(R.id.rvPosts);
+
+        userPosts = new ArrayList<>();
+        //instantiate the adapter
+        userAdapter = new PostAdapter(getContext(), userPosts);
+
+        //set the adapter on the recycler view
+        rvUserPosts.setAdapter(userAdapter);
+
+        //set the layout manager on the recycler view
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        rvUserPosts.setLayoutManager(linearLayoutManager);
+        queryPosts(0);
+
+        swipeContainer = (SwipeRefreshLayout) view.findViewById(R.id.swipeContainer);
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                //get latest 20 Instagram items
+                queryPosts(0);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        // Stop animation (This will be after 2 seconds)
+                        swipeContainer.setRefreshing(false);
+                    }
+                }, 2000); //Delay in millis
+
+            }
+        });
+
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
+
+        scrollListener = new EndlessRecyclerViewScrollListener(linearLayoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                //get the next 20 posts
+                queryPosts(page);
+            }
+        };
+        rvUserPosts.addOnScrollListener(scrollListener);
     }
 
-    @Override
-    protected void queryPosts(int page) {
-        super.queryPosts(page);
+    //Take the posts we have and hand it over to the adapter
+    protected void queryPosts(final int page) {
+        Post.query(page, DISPLAY_LIMIT, filterForUser, new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null){
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+
+                }
+                for(Post post: posts){
+                    Log.i(TAG, "Post: " + post.getDescription() + " Username: " + post.getUser().getUsername());
+                }
+                if(page == 0) {
+                    userAdapter.clear();
+                }
+                userPosts.addAll(posts);
+                userAdapter.notifyDataSetChanged();
+            }
+        });
     }
+
 }
+
