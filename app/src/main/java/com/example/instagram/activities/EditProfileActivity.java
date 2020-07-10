83bb1f0d49b7484337a240ae4f3348b1ab7b8e2e
Patrice Gill <pgill30@fb.com>
2020-07-10 17:13:43 -0400

    Readded item_user_post XML

--- app/src/main/java/com/example/instagram/activities/EditProfileActivity.java
+++ app/src/main/java/com/example/instagram/activities/EditProfileActivity.java
@@ -3,12 +3,23 @@ package com.example.instagram.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.instagram.R;
+import com.parse.ParseFile;
 import com.parse.ParseUser;
 
 public class EditProfileActivity extends AppCompatActivity {
-    ParseUser user;
+    private ParseUser user;
+    private ImageView ivProfilePic;
+    private TextView tvChangePhoto;
+    private EditText etName;
+    private EditText etUsername;
+    private EditText etBio;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +28,34 @@ public class EditProfileActivity extends AppCompatActivity {
 
         //get intent
         user = getIntent().getParcelableExtra("user");
+
+        //find views
+        ivProfilePic = findViewById(R.id.ivProfilePic);
+        tvChangePhoto = findViewById(R.id.tvChangePhoto);
+        etName = findViewById(R.id.etName);
+        etUsername = findViewById(R.id.etUsername);
+        etBio = findViewById(R.id.etBio);
+
+        //bind views with data from user
+        ParseFile image = user.getParseFile("profilePic");
+        Glide.with(this)
+                .load(image.getUrl())
+                .circleCrop()
+                .into(ivProfilePic);
+
+
+        String name = user.getString("name");
+        if (name != null){
+            etName.setText(name);
+        }
+
+        etUsername.setText(user.getUsername());
+
+        String bio = user.getString("bio");
+        if (bio != null){
+            etBio.setText(bio);
+        }
+
+
     }
 }
